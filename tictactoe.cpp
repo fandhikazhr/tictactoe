3e7752e1137c11a51bf332c4c50115cd31746525
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-02-16 01:35:36 +0700

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -116,3 +116,80 @@ void TogglePlayer(){
 	
 
 }
+
+char Win(){
+
+	// first player
+	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+
+	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+
+	// second player
+	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+
+	return '/';
+
+}
+
+int main(){
+	
+	n = 0;
+	Draw();
+	while(1){
+
+		n++;
+		Input();
+		Draw();
+
+		if (Win() == 'X'){
+			cout << "Player 1 or X Wins!" << endl;
+			break;
+		}
+
+		else if (Win() == 'O'){
+			cout << "Player 2 or O Wins!" << endl;
+			break;
+		}
+
+		else if (Win() == '/' && n == 9){
+			cout << "Draw !" << endl;
+			break;
+		}
+		TogglePlayer();
+	}
+	return 0;
+}
